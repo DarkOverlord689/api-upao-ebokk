bb53fddecc8e922ac8a6f19b0edd14954cec8a42
DarkOvs77 <75710806+DarkOverlord689@users.noreply.github.com>
2023-04-24 11:46:27 -0500

    properties ddl

--- api-upao-ebook/src/main/java/com/upao/ebook/domain/Book.java
+++ api-upao-ebook/src/main/java/com/upao/ebook/domain/Book.java
@@ -1,4 +1,14 @@
 package com.upao.ebook.domain;
 
+import java.time.LocalDateTime;
+
 public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private String description;
+    private String imageUrl;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
 }
